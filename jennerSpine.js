eac4be4bf6806887ee7fcea304c993cf44846a51
TranquilityChannel <Acidpants220@gmail.com>
2015-08-08 00:58:31 -0500

    Second Addition to the JennerHTML build

---
+++ jennerSpine.js
@@ -0,0 +1,9 @@
+function = Comb() {
+var combat = [["CR 1", "AC 13", "Touch 12", "Flat Footed 12", "Fortitude +2",  "Reflex +2", "Will +1", "CMD 14", "HP 16", "Ability DC 10", "Spell DC 10", "Ability Modifiers +3, +2, +1", "Skills One Master (+8), Two Good (+6)", "1 Combat Option"]];
+
+}
+
+var combatTwo = {challengeRating:"1", armorClass:"13", touchArmorClass:"12", flatFootedArmorClass:"12", fortitudeSave:"+2", reflexSave:"+2", willSave:"+1", combatManeuverDefense:"14", hitPoints:"16", abilityDC:"10", spellDC:"10"}
+
+function = getMainStats()
+alert("Bunnies")
